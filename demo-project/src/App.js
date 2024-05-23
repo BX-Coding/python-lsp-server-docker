e716923f59619ef9b1c754d8f0c24e3a161c122e
Leighton Miguel <leim56@hawaii.rr.com>
2024-05-22 14:58:27 -1000

    added demo project

---
+++ demo-project/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import { EditorView } from "codemirror";
+import { EditorState } from "@codemirror/state";
+import { languageServer } from "codemirror-languageserver";
+import { basicSetup } from "codemirror";
+import { python } from "@codemirror/lang-python";
+import { lintGutter } from "@codemirror/lint";
+import { indentationMarkers } from "@replit/codemirror-indentation-markers";
+
+function App() {
+  const editor = useRef(null);
+
+  useEffect(() => {
+    const serverUri = "ws://localhost:8000";
+
+    const ls = languageServer({
+      serverUri,
+      rootUri: "file:///",
+      documentUri: "file:///index.js",
+      languageId: "python",
+    });
+
+    const startState = EditorState.create({
+      doc: "# Write your Python code here",
+      extensions: [
+        basicSetup,
+        ls,
+        python(),
+        lintGutter(),
+        indentationMarkers(),
+      ],
+    });
+
+    const view = new EditorView({
+      state: startState,
+      parent: editor.current,
+    });
+
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Python Code Editor</h1>
+      <div ref={editor}></div>
+    </div>
+  );
+}
+
+export default App;
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 This repository is a dockerfile that builds a docker image of the python language server using LSP protocol.
 
 python-lsp-server can be found here : https://github.com/python-lsp/python-lsp-server
+
+# Demo project
+
+In the demo-project subfolder, there is a simple react app that you can use to test the websocket URL of the container.
+To run the demo project, cd into the demo-project folder, install the dependencies with ``npm install`` then ```npm start``` to start the app. In App.js, change the const serverUri to the websocket uri which should be on port 8000.
